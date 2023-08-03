2fac962eafe2da8b0ff6e274a0e4ab40a5e54d46
Himanshu Kumar <69851575+Himanshu-kr-007@users.noreply.github.com>
2023-08-03 16:44:34 +0530

    Update Providers.tf

--- Team-Dev/Providers.tf
+++ Team-Dev/Providers.tf
@@ -9,7 +9,6 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
   # profile = "default"
-  access_key = "AKIAQRXKLV2ODLXUHAOI"
-  secret_key = "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"
+
 
 }
